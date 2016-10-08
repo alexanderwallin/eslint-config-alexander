3d102476f92025b0f29fd587bd28fb3a415ed60f
Alexander Wallin <office@alexanderwallin.com>
2016-10-08 16:18:32 +0100

    Adds eslint-plugin-import with configs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['xo'],
+  plugins: ['import'],
   rules: {
     'arrow-parens': 0,
     'brace-style': ['error', 'stroustrup'],
@@ -11,5 +12,22 @@ module.exports = {
     'padded-blocks': 0,
     'radix': ['error', 'as-needed'],
     'semi': ['error', 'never'],
+
+    // import
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/default': 'error',
+    'import/namespace': 'error',
+    'import/no-dynamic-require': 'error',
+
+    'import/export': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-deprecated': 'warn',
+
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': 'error',
+    'import/newline-after-import': 'error',
   }
 }
