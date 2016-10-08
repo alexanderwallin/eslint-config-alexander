3ed2a923a2efe01f9a71fda4cc9a9367cba4ee1f
Alexander Wallin <office@alexanderwallin.com>
2016-10-08 15:48:26 +0100

    First config, based on xo

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['xo'],
+  rules: {
+    'arrow-parens': 0,
+    'brace-style': ['error', 'stroustrup'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': ['error', 2],
+    'no-use-before-define': 0,
+    'object-curly-spacing': ['error', 'always'],
+    'operator-linebreak': ['error', 'before'],
+    'padded-blocks': 0,
+    'radix': ['error', 'as-needed'],
+    'semi': ['error', 'never'],
+  }
+}
